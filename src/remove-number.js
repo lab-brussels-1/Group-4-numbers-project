d239cc8012fd477b716954ffc4b05c4dbb88f3b7
fenny wilriani <fenny.wilriani@gmail.com>
2022-06-13 10:17:03 +0200

    remove num

--- src/remove-number.js
+++ src/remove-number.js
@@ -8,10 +8,17 @@ const removeNumber = () => {
   debugger;
 
   // read the user's number
+  const newNumber = readNumber('the-num');
 
   // remove the user's number from the array
+  numbers.pop(newNumber);
 
   // display the new list of numbers (use the util function!)
+  let numbersList = '';
+  for (const num of numbers) {
+    numbersList = `# ${num}\n`;
+  }
+  display('your-output',numbersList);
 };
 
 // !! fill in this blank with the correct DOM ID
