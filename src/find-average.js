3b9d7e4e3b92df0eae01c7f0de4a3022954016e8
NazgulM <maniyasova.n@gmail.com>
2022-06-14 21:54:56 +0200

    find average

--- src/find-average.js
+++ src/find-average.js
@@ -2,15 +2,20 @@ import { numbers } from '../numbers.js';
 
 import { display } from '../lib/dom-io.js';
 
-import { average } from './utils/average.js';
+// import { average } from './utils/average.js';
 
 const findAverage = () => {
-  debugger;
+
 
   // calculate the average of all saved numbers  (use your util function!)
-
+  let sum = 0;
+for (let i of numbers) {
+sum += i;
+}
+const avg =  sum/numbers.length
   // display the average in the UI
+  display('your-output', 'average: ' + avg);
 };
 
 // !! fill in this blank with the correct DOM ID
-document.getElementById('__').addEventListener('click', findAverage);
+document.getElementById('average-them').addEventListener('click', findAverage);
