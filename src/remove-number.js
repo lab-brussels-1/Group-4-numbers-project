afbbc8bf6961d828b4236eb6077a775ed4ae6b98
NazgulM <maniyasova.n@gmail.com>
2022-06-14 21:22:38 +0200

    remove  number

--- src/remove-number.js
+++ src/remove-number.js
@@ -5,14 +5,20 @@ import { readNumber, display } from '../lib/dom-io.js';
 import { listItems } from './utils/list-items.js';
 
 const removeNumber = () => {
-  debugger;
-
   // read the user's number
+  const number1 = readNumber('the-num');
 
   // remove the user's number from the array
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === number1) {
+      numbers.splice(i, 1);
+      --i;
+    }
+  }
 
   // display the new list of numbers (use the util function!)
+  display('your-output', listItems(numbers));
 };
 
 // !! fill in this blank with the correct DOM ID
-document.getElementById('__').addEventListener('click', removeNumber);
+document.getElementById('remove-it').addEventListener('click', removeNumber);
--- src/show-numbers.js
+++ src/show-numbers.js
@@ -8,9 +8,9 @@ const showNumbers = () => {
   debugger;
 
   // generate a string list of all numbers (use the util!)
-
+  display('your-output', listItems(numbers));
   // display the list of numbers to the UI
 };
 
 // !! fill in this blank with the correct DOM ID
-document.getElementById('__').addEventListener('click', showNumbers);
+document.getElementById('show-them').addEventListener('click', showNumbers);
