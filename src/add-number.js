8c416527c11f71794610f029d1cf0530ff749b1b
NazgulM <maniyasova.n@gmail.com>
2022-06-14 13:38:23 +0200

    npm

--- src/add-number.js
+++ src/add-number.js
@@ -4,8 +4,6 @@ import { readNumber, display } from '../lib/dom-io.js';
 
 import { listItems } from './utils/list-items.js';
 
-
-
 const addNumber = () => {
   // read the user's number
   const number1 = readNumber('the-num');
