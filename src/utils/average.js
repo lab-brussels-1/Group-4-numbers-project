70d0bd8d8d5f705e065adb1e76008af41075fd07
NazgulM <maniyasova.n@gmail.com>
2022-06-14 23:03:58 +0200

    error

--- src/utils/average.js
+++ src/utils/average.js
@@ -1,8 +1,8 @@
 /* eslint-disable jsdoc/require-returns-check */
 /**
  * Calculates the average of a list of numbers.
-  *
-  * @param {number[]} [nums=[]] - The numbers to average.
-  * @returns {number} The average of all numbers.
-  */
+ *
+ * @param {number[]} [nums=[]] - The numbers to average.
+ * @returns {number} The average of all numbers.
+ */
 export const average = (nums = []) => {};
