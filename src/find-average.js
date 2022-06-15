9a993a9c1deca71c8bb35fb39e06d518c7bb2f21
fenny wilriani <fenny.wilriani@gmail.com>
2022-06-15 10:36:38 +0200

    average num

--- src/find-average.js
+++ src/find-average.js
@@ -8,9 +8,13 @@ const findAverage = () => {
   debugger;
 
   // calculate the average of all saved numbers  (use your util function!)
+  const sum = numbers.reduce((a, b) => a + b);
 
+  // return Math.floor(sum / numbers.length);
   // display the average in the UI
-};
+  const average1 = Math.floor(sum / numbers.length);
 
+  display('your-output', average1);
+};
 // !! fill in this blank with the correct DOM ID
-document.getElementById('__').addEventListener('click', findAverage);
+document.getElementById('average-them').addEventListener('click', findAverage);
